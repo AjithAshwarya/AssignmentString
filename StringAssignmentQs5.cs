07f2b88da4df22ca82d8681756c3de3f9bcdd01c
AiswaryaAjith <ajithashwarya@gmail.com>
2025-01-20 08:59:10 +0530

    Add files via upload

---
+++ StringAssignmentQs5.cs
@@ -0,0 +1,42 @@
+﻿namespace AssignmentString5
+{
+    internal class StringAssignmentQs5
+    {
+        static void Main(string[] args)
+        {
+            // Ask user to input a string
+            Console.WriteLine("Enter a string:");
+            string inputStr = Console.ReadLine();
+
+            // Initialize counters for alphabets, digits, and special characters
+            int alphabetCount = 0;
+            int digitCount = 0;
+            int specialCharCount = 0;
+
+            // Loop through each character in the input string
+            foreach (char inputChar in inputStr)
+            {
+                // Checking for Alphabet
+                if ((inputChar >= 65 && inputChar <= 90) || (inputChar >= 97 && inputChar <= 122))
+                {
+                    alphabetCount++;
+                }
+                // Checking for Digits
+                else if (inputChar >= 48 && inputChar <= 57)
+                {
+                    digitCount++;
+                }
+                // Otherwise Special Character
+                else
+                {
+                    specialCharCount++;
+                }
+            }
+
+            // Output the counts
+            Console.WriteLine($"Total Alphabets: {alphabetCount}");
+            Console.WriteLine($"Total Digits: {digitCount}");
+            Console.WriteLine($"Total Special Characters: {specialCharCount}");
+        }
+    }
+}
