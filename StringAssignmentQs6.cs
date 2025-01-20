1d1c5d975c4053b10a80ebbb8145c38b7009808f
AiswaryaAjith <ajithashwarya@gmail.com>
2025-01-20 08:59:47 +0530

    Add files via upload

---
+++ StringAssignmentQs6.cs
@@ -0,0 +1,32 @@
+﻿namespace AssignmentString6
+{
+    internal class StringAssignmentQs6
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user to input a string
+            Console.WriteLine("Enter a string");
+            String inputStr = Console.ReadLine();
+
+            // Ask the user to input an index value to start the substring
+            Console.WriteLine("Enter the index value");
+            int inputIndexValue;
+
+            // Try parsing the input index value to ensure it's a valid integer
+            int.TryParse(Console.ReadLine(), out inputIndexValue);
+
+            // Initialize an empty string to hold the resulting substring
+            String subString = "";
+
+            // Loop through the string starting from the input index to the end of the string
+            for (int i = inputIndexValue; i < inputStr.Length; i++)
+            {
+                // Concatenate each character starting from the input index to subString
+                subString += inputStr[i];
+            }
+
+            // Output the resulting substring
+            Console.WriteLine($"The substring is {subString}");
+        }
+    }
+}
